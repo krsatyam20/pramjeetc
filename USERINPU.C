d6ff609471a02e0a94ab0f304ce3ff545b8142fa
satyam <39213202+krsatyam20@users.noreply.github.com>
2020-02-15 10:42:07 +0530

    Add files via upload

---
+++ USERINPU.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+clrscr();
+
+//Get Value From User
+
+int a,b,c;
+
+printf("Please enter the first value");
+scanf("%d",&a);
+printf("please enter the second Value");
+scanf("%d",&b);
+c=a+b;
+
+printf("%d",c);
+
+
+
+
+
+
+
+getch();
+}
